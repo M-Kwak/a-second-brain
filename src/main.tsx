958e36e65ac22a5d1e629f308a55a0fe1b96231f
M-Kwak <prospirev@gmail.com>
2024-07-31 22:22:00 +0200

    Clean react boilerplate for further devlopement by : - Deleting App.tsx and App.css - Creating styles, pages and components directory - Merging App.css and index.css in styles.css - Seting up basic Login layout component

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './styles/styles.css';
+import Login from './pages/login/Login.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Login />
   </React.StrictMode>,
 )
