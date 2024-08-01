516f87caea2910e68de847b7d2931f5a9e705fff
M-Kwak <prospirev@gmail.com>
2024-08-01 21:20:17 +0200

    Import react-router-dom package. Add router in main.tsx and temporarily set Login component as default root.

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/styles.css';
 import Login from './pages/login/Login.tsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Login />
+    <RouterProvider 
+      router={router}
+    />
   </React.StrictMode>,
 )
