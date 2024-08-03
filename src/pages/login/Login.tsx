f5ec9c07ece7f4d1417777df76a14e4e7a00222b
M-Kwak <prospirev@gmail.com>
2024-08-03 21:37:28 +0200

    Add html content to Login page. (#2)
    * Add html content to Login page.
    
    Create flex Container component.
    Set minimum width and minimum heigh to root container.
    
    * Add Nunito-Sans and Roboto fonts

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,8 +1,24 @@
-function Login() {
+import Container from "../../components/container/Container";
+
+function Login(): React.JSX.Element {
   return (
-    <div>
-      Login
-    </div>
+    <Container direction="column">
+      <Container direction="column">
+        <h1>Login</h1>
+      </Container>
+      <Container direction="column">
+        <input type="text" placeholder="Email" />
+        <input type="text" placeholder="Password" />
+        <span>Forgot Password ?</span>
+      </Container>
+      <Container direction="column">
+        <button>Login</button>
+        <Container direction="row">
+          <p>Don't have an account yet ?</p>
+          <span>Create</span>
+        </Container>
+      </Container>
+    </Container>
   );
 }
 
